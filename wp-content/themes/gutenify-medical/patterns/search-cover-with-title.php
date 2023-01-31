2e7ef7e8121ca847a800496af5fafeecbb7616f9
Martín Matayoshi <mmatayoshiy@gmail.com>
2023-01-31 16:49:27 -0300

    01
    demo

---
+++ wp-content/themes/gutenify-medical/patterns/search-cover-with-title.php
@@ -0,0 +1,17 @@
+<?php
+ /**
+  * Title: Search Cover With Title
+  * Slug: gutenify-medical/search-cover-with-title
+  * Categories: gutenify-medical
+  */
+?>
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() );?>/images/cta-banner.jpg","id":383,"dimRatio":40,"overlayColor":"foreground","focalPoint":{"x":0.4,"y":0.81},"minHeight":496,"minHeightUnit":"px","isDark":false} -->
+<div class="wp-block-cover is-light" style="min-height:496px"><span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-40 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-383" alt="" src="<?php echo esc_url( get_template_directory_uri() );?>/images/cta-banner.jpg" style="object-position:40% 81%" data-object-fit="cover" data-object-position="40% 81%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:post-title {"textAlign":"center","level":1,"align":"wide","textColor":"background","fontSize":"large"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"0px"}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-bottom:0px"><!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","align":"center","backgroundColor":"primary","textColor":"light"} /--></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
